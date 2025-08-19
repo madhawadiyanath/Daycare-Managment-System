82efa451afc0549eceb61802db938953692d74f6
madhawadiyanath <madhawadiyanth778@gmail.com>
2025-08-19 16:48:11 +0530

    w

--- backend/app.js
+++ backend/app.js
@@ -1,2 +1,13 @@
-console.log("bye");
-console.log("bye");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://<db_username>:ywGSaGThVLxZvnQr@cluster0.sfvhdit.mongodb.net/")
+
+ywGSaGThVLxZvnQr
