f1c423a1a738ef46d86d14844cc486b0f1509ef9
madhawadiyanath <madhawadiyanth778@gmail.com>
2025-08-21 16:12:56 +0530

    j

--- backend/app.js
+++ backend/app.js
@@ -4,27 +4,21 @@ const cors = require("cors");
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
   res.send("It is working");
 });
 
-// Corrected MongoDB connection
-mongoose.connect("mongodb+srv://admin:uktNIVQQ0X5MfySV@cluster0.8kk63n7.mongodb.net/")
+// Test connection without starting server first
+mongoose.connect("mongodb+srv://admin:5YdcKV1qUqM18Gkv@cluster0.8kk63n7.mongodb.net/daycare_db?retryWrites=true&w=majority")
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5000, (err) => {
-      if (err) {
-        console.error("Server startup error:", err);
-      } else {
-        console.log("Server running on port 5000");
-      }
+    app.listen(5000, () => {
+      console.log("Server running on port 5000");
     });
   })
   .catch((err) => {
-    console.error("MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err.message);
   });
